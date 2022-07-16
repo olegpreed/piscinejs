a9f944767cccd9e0cfbdb4c682d115bc7e1ebc55
Oleg Preed <leo.silveri@mail.ru>
2022-07-16 13:54:41 +0300

    CLASSES

---
+++ classes.js
@@ -0,0 +1,36 @@
+/*
+
+У экземпляра класса должны присутствовать св-ва:
+-name string
+-grade string Для простоты предположим, что система грейдов будет иметь значения от L1 до L4
+-hardSkills string[]
+-company string
+
+
+Так же должны иметься три метода:
+
+-changeCompany(newCompanyName) - сотрудник может сменить компанию, либо же просто уволиться
+-upGrade(newGradeName) - сотрудник может повысить квалификацию
+-addSkill(newSkillName) - сотрудник может дополнить список своих скиллов.
+*/
+
+
+export class Employee { 
+	constructor(name, grade, skills, company) {
+	this.name = name;
+	this.grade = grade;
+	this.skills = skills;
+	this.company = company;
+	}
+	changecompany(newCompanyName) {
+		this.company = newCompanyName;
+	}
+	upGrade(newGradeName) {
+		this.grade = newGradeName;
+	}
+	addSkill(newSkillName) {
+		this.skills.push(newSkillName)
+	}
+}
+
+
